22a2323814a96c359efcbaf878002a127b738a23
sanctum006 <74344331+sanctum006@users.noreply.github.com>
2021-08-17 19:07:14 +0530

    Delete firebase.js

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBkFohiMX-LXTI684ZRRmCA3H_GT1AzCKs",
-  authDomain: "project-sayf-000.firebaseapp.com",
-  projectId: "project-sayf-000",
-  storageBucket: "project-sayf-000.appspot.com",
-  messagingSenderId: "1085320112481",
-  appId: "1:1085320112481:web:caf084fd681a8042397d20",
-};
