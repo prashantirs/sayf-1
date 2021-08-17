94d18c206f21b37fdafc2365010aa3ef8def0c66
sanctum007 <pushpendrayadav1057@gmail.com>
2021-08-18 02:32:25 +0530

    added join wishlist component

--- src/Dummy/Dummy.jsx
+++ src/Dummy/Dummy.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Dummy() {
-  return <div>Main App</div>;
+  return (
+    <div>
+      <Link to="/join-waitlist">Main App</Link>
+    </div>
+  );
 }
 
 export default Dummy;
--- src/JoinWaitlist/JoinWaitlist.jsx
+++ src/JoinWaitlist/JoinWaitlist.jsx
@@ -1,8 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, Redirect, useHistory } from "react-router-dom";
 import "./JoinWaitlist.css";
+import logo from "./../assets/logo.png";
+import leftArrow from "./../assets/left-arrow.svg";
 
 function JoinWaitlist() {
-  return <div className="joinWaitlist__container">Hello</div>;
+  let history = useHistory();
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+  });
+
+  const handleChange = (e) => {
+    setUserData((s) => ({ ...s, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userData);
+    history.push("/");
+  };
+
+  return (
+    <div className="joinWaitlist__container">
+      <div className="joinWaitlist__topContainer">
+        <div className="joinWaitlist__topContainerLeft">
+          <div className="joinWaitlist__textContent">
+            <Link to="/">
+              <button className="joinWaitlist__leftArrow">
+                <img src={leftArrow} alt="" />
+              </button>
+            </Link>
+            <h1 className="joinWaitlist__textContentHeader">
+              Join in under 2 minutes
+            </h1>
+            <p>
+              Once you’re in, you’ll get a sneak peek into your spends since
+              2020.
+            </p>
+          </div>
+          <form action="" className="joinWaitlist__form">
+            <input
+              type="text"
+              placeholder="FULL NAME"
+              className="joinWaitlist__input"
+              name="name"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="EMAIL"
+              className="joinWaitlist__input"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="MOBILE NO"
+              className="joinWaitlist__input"
+              name="mobile"
+              onChange={handleChange}
+            />
+            <button
+              type="submit"
+              className="joinWaitlist__submitBtn"
+              onClick={handleSubmit}
+            >
+              Join Waitlist
+            </button>
+          </form>
+        </div>
+        <div className="joinWaitlist__topContainerRight">
+          <img className="joinWaitlist__logo" src={logo} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default JoinWaitlist;
