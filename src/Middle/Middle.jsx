8b06617069264933775bb021aa6996e9f598a904
Prashant Srivastava <prashantsrivastava381@gmail.com>
2021-08-23 14:26:09 +0530

    box Shadow add in contact form

--- src/Middle/Middle.jsx
+++ src/Middle/Middle.jsx
@@ -6,13 +6,13 @@ function Middle() {
   return (
     <div className="middle__container">
       <div className="middle__leftContainer">
-        <h1 className="middle__heading">
-          Join our wait list to get early access to the app SayF
-        </h1>
-        <h2 className="middle__subHeading">
+        <div className="middle__heading">
+          Join our wait list to get early access to the app <span className="brandName">SayF</span>
+        </div>
+        <div className="middle__subHeading">
           SayF is a personal finance app that helps you save, invest and
           optimize your spending for a better future
-        </h2>
+        </div>
         <Link to="/join-waitlist">
           <button
             className="btn btn-outline-success middle__getEarlyAccessButton"
